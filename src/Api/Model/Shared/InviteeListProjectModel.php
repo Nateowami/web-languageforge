72b50fdb48981ce457d1109cef6e797a8e7ff394
Nathaniel Paulus <nateowami@gmail.com>
2018-07-26 18:30:12 -0400

    WIP

--- src/Api/Model/Shared/InviteeListProjectModel.php
+++ src/Api/Model/Shared/InviteeListProjectModel.php
@@ -8,18 +8,18 @@ use Api\Model\Shared\Mapper\MongoMapper;
 /**
  * List of users who are members of the specified project
  */
-class UserListProjectModel extends MapperListModel
+class InviteeListProjectModel extends MapperListModel
 {
     /**
-     * UserListProjectModel constructor.
+     * InviteeListProjectModel constructor.
      * @param string $projectId
      */
     public function __construct($projectId)
     {
         parent::__construct(
                 UserModelMongoMapper::instance(),
-                array('username' => array('$regex' => '\w'), 'projects' => array('$in' => array(MongoMapper::mongoID($projectId)))),
-                array('username', 'email', 'name') // TODO Stop exposing email this way
+                array('last_login' => array('$exists' => false), 'projects' => array('$in' => array(MongoMapper::mongoID($projectId)))),
+                array('username', 'email', 'name')
         );
     }
 }
